ed7e601ad78c428720e400d0dd5476fa7aa40af5
Parama Fawwaz <ParamaFawwaz@student.tudelft.nl>
2022-03-01 16:10:37 +0100

    finished ver 1 testbench

--- timebase_tb.vhdl
+++ timebase_tb.vhdl
@@ -9,3 +9,23 @@ architecture structural of testbench is
         port(   clk     : in std_logic;
                 reset   : in std_logic;
                 count_out: out std_logic);
+            );
+    end component timebase;
+
+    signal clk      : std_logic;
+    signal reset    : std_logic;
+    signal count_out: std_logic;
+
+begin 
+        clk     <=  '1' after 0 ns, '0' after 1 ns when clk /= '0', else '1' after 1ns;
+        reset   <=  '1' after 0 ns, 
+                    '0' after 2 ns --to test if reset will not affect it 
+                    '1' after 800000 ns; -- to test if reset works after at a later point in time
+
+lbl:    timebase port map (     clk => clk,
+                                reset => reset,
+                                count_out => count_out
+);       
+end architecture structural;
+
+        
