19651d3b6bedac85b9f10e41421759d00f180cc1
Parama Fawwaz <ParamaFawwaz@student.tudelft.nl>
2022-03-02 11:59:59 +0100

    Update motorcontrol.vhdl

--- motorcontrol.vhdl
+++ motorcontrol.vhdl
@@ -6,13 +6,23 @@ entity motorcontrol is
 	port (	clk		: in	std_logic;
 		reset		: in	std_logic;
 		direction	: in	std_logic;
-		count_in	: in	std_logic_vector 20 downto 0);  -- Please enter upper bound
+		count_in	: in	std_logic_vector (20 downto 0);  -- Please enter upper bound
 
 		pwm		: out	std_logic
 	);
 end entity motorcontrol;
 
 architecture behavioral of motorcontrol is
+	
+	type
+	signal state, new_state: std_logic_vector (2 downto 0);
+
 	begin
-	process(clk)
-		if ()
+	process(state, direction)
+	begin
+		case state is
+			when "00" => 
+			when '01' =>
+			when '10'=>
+			
+			
